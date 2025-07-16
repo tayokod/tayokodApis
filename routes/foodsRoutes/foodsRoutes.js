23681af0f78879b8ec6fbf100064bc8e9a30c665
tayokod <tayokoddevelopers@gmail.com>
2025-07-16 09:48:31 +0300

    done

--- routes/foodsRoutes/foodsRoutes.js
+++ routes/foodsRoutes/foodsRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { GetFoodsController, PostFoodsController } from '../../controllers/foodsController/foodsController.js';
+import { GetFoodByIdController, GetFoodsController, PostFoodsController } from '../../controllers/foodsController/foodsController.js';
 
 const router = express.Router();
 
 router.get('/foods', GetFoodsController);
 router.post('/foods', PostFoodsController);
+
+// get food by id
+router.get('/foods/:id', GetFoodByIdController);
 
 
 
